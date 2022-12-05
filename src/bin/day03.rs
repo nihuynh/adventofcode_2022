f419ee4f9bd65c8d1cce7380d0af7d689004bf92
Nicolas Huynh <nico.huynh@gmail.com>
2022-12-05 20:25:45 +0100

    day3

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,15 +1,10 @@
-// struct Job {
-//     start: i32,
-//     end: i32,
-// }
-
-// impl Job {
-//     fn parse() -> Self {
-//         Job { start: 30, end: 32 }
-//     }
-// }
-
 use common::read_lines;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>()
+    };
+}
 
 fn main() {
     let mut score = 0;
@@ -21,7 +16,6 @@ fn main() {
                 if check_overlap(pair) {
                     score += 1;
                 }
-                // let job: Vec<Vec<char>> = pair.iter().map(|)split('-').collect;
             }
         }
         println!("The score is : {}", score);
@@ -33,8 +27,17 @@ fn main() {
 fn check_overlap(pair: Vec<&str>) -> bool {
     let a: Vec<&str> = pair[0].split('-').collect();
     let b: Vec<&str> = pair[1].split('-').collect();
-    println!("A: {:#?} and B: {:#?}", a, b);
-    // let process_a: Vec<i32> = a.iter().map(|s| s[0] as i32).collect();
-    // println!("data : {:#?}", process_a);
-    true
+    let a_num: Vec<i32> = a.iter().map(|s| parse_input!(s, i32).unwrap()).collect();
+    let b_num: Vec<i32> = b.iter().map(|s| parse_input!(s, i32).unwrap()).collect();
+    // println!("A : {:#?} B : {:#?}", a_num, b_num);
+    // First star:
+    // (is_in_range(a_num[0], b_num[0], b_num[1]) && is_in_range(a_num[1], b_num[0], b_num[1]))
+    //     || (is_in_range(b_num[0], a_num[0], a_num[1]) && is_in_range(b_num[1], a_num[0], a_num[1]))
+    // Second star:
+    (is_in_range(a_num[0], b_num[0], b_num[1]) || is_in_range(a_num[1], b_num[0], b_num[1]))
+        || (is_in_range(b_num[0], a_num[0], a_num[1]) || is_in_range(b_num[1], a_num[0], a_num[1]))
+}
+
+fn is_in_range(value: i32, min: i32, max: i32) -> bool {
+    value >= min && value <= max
 }
